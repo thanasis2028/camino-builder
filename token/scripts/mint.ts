17e548eb3090d1fe1824a4e4744aae137c25a053
Athanasios Katsios <aka@anixe.io>
2024-01-27 18:55:25 +0200

    Add the basic application

--- token/scripts/mint.ts
+++ token/scripts/mint.ts
@@ -1,7 +1,7 @@
 import {ethers} from "hardhat"
 
 async function main() {
-    const deployedToken = await ethers.getContractAt("TestToken", "0x2a5FA0098Bb73DC9322342100f278cAdBf844e42")
+    const deployedToken = await ethers.getContractAt("GreenOnionToken", "0x2a5FA0098Bb73DC9322342100f278cAdBf844e42")
 
     // await deployedToken.transfer("c-chain address", ethers.utils.parseEther("42"));
     await deployedToken.transfer("0xfc0d60d398F083D00117F6cdd665a8B4693Be8a3", ethers.utils.parseEther("15"));
